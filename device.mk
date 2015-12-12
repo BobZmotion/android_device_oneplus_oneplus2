a31ed44002f566a67dc5fe95c8f92a4b17f69984
BobZhome <BobZhome@cheerful.com>
2015-12-12 11:45:50 -0500

    sound: add a couple of favorites

--- device.mk
+++ device.mk
@@ -244,6 +244,12 @@ PRODUCT_PACKAGES += \
     sensors.msm8994 \
     sensors.ssc.wrapper
 
+# Sound configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/Heaven.ogg:system/media/audio/notifications/Heaven.ogg \
+    $(LOCAL_PATH)/prebuilt/somebodys.ogg:system/media/audio/ringtones/somebodys.ogg
+
+# Thermal config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf
 
