82fd675e2777c7e87352ab5830da04bff7479863
Peter Qiu <zqiu@google.com>
2017-05-05 14:00:40 -0700

    oneplus2: Use hardware feature definition to indicate Passpoint support
    Bug: 36886243
    Test: Manual test
    Change-Id: I570f7037abfd670e153c62a8dc723f1e89e9bf56

--- device.mk
+++ device.mk
@@ -51,6 +51,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.aware.xml:system/etc/permissions/android.hardware.wifi.aware.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:system/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
 # Boot animation
